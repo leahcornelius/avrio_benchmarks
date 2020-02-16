2863c24a31313f89b6dd9f39b30fb224bb99e606
Leo Cornelius <56726537+leocornelius@users.noreply.github.com>
2020-02-16 20:38:53 +0000

    Update main.rs

--- transaction/src/main.rs
+++ transaction/src/main.rs
@@ -15,10 +15,30 @@ pub struct Transaction {
     pub gas_price: u64,
     pub max_gas: u64,
     pub gas: u64, // gas used
-    pub nonce: u8,
+    pub nonce: u64,
     pub signature: String,
 }
-
+fn generateTransactions(amount: u64) -> Vec<Transaction> {
+    let mut txns: Vec<Transaction> = vec![];
+    for 0..=amount {
+        let txn = Transaction { 
+            amount: 10, 
+            extra: String::from(""), 
+            flag: '', 
+            sender_key: String::from(hash(String::from("skey..."))),
+            receive_key: String::from(hash(String::from("rc"))), 
+            access_key: String::from(hash(String::from("sk"))),
+            gas_price: 10,
+            max_gas: 100,
+            gas: 20,
+            nonce: 2378375,
+            signature: String::from(hash(String::from("sig...")));
+        };
+        txn.hash();
+        txns.push(txn);
+    }
+    return txns;
+}
 impl Transaction {
     fn typeTransaction(&self) -> String {
         return match (self.extra) {
@@ -62,17 +82,24 @@ impl Transaction {
     fn bytes(&self) -> Vec<u8> {
         let mut bytes = vec![];
 
-        bytes.extend(self.ammount);
-        bytes.extend(self.extra);
-        bytes.extend(self.flag);
+        bytes.extend(String::from(self.amount.to_owned()));
+        bytes.extend(String::from(self.extra.to_owned));
+        bytes.extend(String::from(self.flag.to_owned()));
         bytes.extend(self.sender_key);
         bytes.extend(self.receive_key);
-        bytes.extend(self.gas * self.gas_price); // aka fee
-        bytes.extend(self.nonce);
+        bytes.extend(String::from(self.gas * self.gas_price.to_owned())); // aka fee
+        bytes.extend(String::from(self.nonce.to_owned()));
         bytes
     }
     fn hash(&self) {
         let asbytes = self.bytes();
         let out = cryptonight(&asbytes, asbytes.len(), 0);
         self.hash = hex::encode(out);
+    }
+}
+
+fn hash(subject: String) -> String{
+    let asbytes = subject.as_bytes();
+    let out = cryptonight(&asbytes, asbytes.len(), 0);
+    return hex::encode(out);
 }
