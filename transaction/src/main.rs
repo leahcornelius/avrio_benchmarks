9ac02827c409fd266e8b374557b4737d48522620
Leo <leo@helives.mine2.live>
2020-02-16 17:54:37 -0500

    Add working code

--- transaction/src/main.rs
+++ transaction/src/main.rs
@@ -2,6 +2,9 @@ use serde::{Deserialize, Serialize};
 extern crate hex;
 extern crate cryptonight;
 use cryptonight::cryptonight;
+extern crate rand;
+use rand::Rng;
+use std::time::{Duration, Instant};
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Transaction {
@@ -18,29 +21,34 @@ pub struct Transaction {
     pub nonce: u64,
     pub signature: String,
 }
-fn generateTransactions(amount: u64) -> Vec<Transaction> {
+fn gen(amount: u64) -> Vec<Transaction> {
+    let mut i: u64 = 0;
     let mut txns: Vec<Transaction> = vec![];
-    for 0..=amount {
-        let txn = Transaction { 
-            amount: 10, 
+    let mut rng = rand::thread_rng();
+    while i < amount {
+        let mut txn = Transaction { 
+            hash: String::from("hash"),
+            amount: rng.gen(), 
             extra: String::from(""), 
-            flag: '', 
-            sender_key: String::from(hash(String::from("skey..."))),
-            receive_key: String::from(hash(String::from("rc"))), 
-            access_key: String::from(hash(String::from("sk"))),
-            gas_price: 10,
-            max_gas: 100,
-            gas: 20,
-            nonce: 2378375,
-            signature: String::from(hash(String::from("sig...")));
+            flag: 'n', 
+            sender_key: (hash(String::from("skey...".to_owned() + &rng.gen::<u64>().to_string()))),
+            receive_key: (hash(String::from("rc".to_owned() + &rng.gen::<u64>().to_string()))),
+            access_key: (hash(String::from("sk".to_owned() + &rng.gen::<u64>().to_string()))),
+            gas_price: rng.gen::<u16>() as u64,
+            max_gas: rng.gen::<u16>() as u64,
+            gas: rng.gen::<u16>() as u64,
+            nonce: rng.gen(),
+            signature: String::from(hash(String::from("sig...".to_owned() + &rng.gen::<u64>().to_string()))),
         };
         txn.hash();
+        println!(" {:?}", i);
         txns.push(txn);
+        i += 1;
     }
     return txns;
 }
 impl Transaction {
-    fn typeTransaction(&self) -> String {
+/*    fn typeTransaction(&self) -> String {
         return match (self.extra) {
             "" => "normal",
             "r" => "reward",
@@ -50,7 +58,7 @@ impl Transaction {
             "b" => "burn",
             _ => "message",
         };
-    }
+    }*/
 
     fn validateTransaction(&self) -> bool {
         /* assume ammount is correct for benchmark
@@ -59,47 +67,84 @@ impl Transaction {
             return false;
         }
         */
-        if self.amount < 0.0001 {
+        if self.amount < 1 {
             // the min amount sendable (1 miao)
             return false;
         }
-        if self.access_key != sender_key {
-            if  100000 < self.amount {
+        if self.access_key != self.sender_key {
+            if  0 > self.amount {
                 return false;
-            else if self.hash() != self.hash {
+            } else if self.hashReturn() != self.hash {
                 return false;
             }
             else if self.extra.len() > 100 {
                 return false;
             }
-            } else {
+             else {
                 if self.signature != "" {
                     return true;
                 }
             }
         }
+    return true;
     }
     fn bytes(&self) -> Vec<u8> {
         let mut bytes = vec![];
 
-        bytes.extend(String::from(self.amount.to_owned()));
-        bytes.extend(String::from(self.extra.to_owned));
-        bytes.extend(String::from(self.flag.to_owned()));
-        bytes.extend(self.sender_key);
-        bytes.extend(self.receive_key);
-        bytes.extend(String::from(self.gas * self.gas_price.to_owned())); // aka fee
-        bytes.extend(String::from(self.nonce.to_owned()));
+        bytes.extend((self.amount.to_string()).bytes());
+        bytes.extend((self.extra.to_owned()).bytes());;
+        bytes.extend(self.flag.to_string().bytes());
+        bytes.extend(self.sender_key.bytes());
+        bytes.extend(self.receive_key.bytes());
+        bytes.extend(((self.gas * self.gas_price.to_owned()).to_string()).bytes()); // aka fee
+        bytes.extend((self.nonce.to_owned().to_string()).bytes());
         bytes
     }
-    fn hash(&self) {
+    fn hash(&mut self) {
         let asbytes = self.bytes();
+        unsafe {
+            let out = cryptonight(&asbytes, asbytes.len(), 0);
+        
+            self.hash = hex::encode(out);
+        }
+    }
+    fn hashReturn(&self) -> String {
+        let asbytes = self.bytes();
+        unsafe {
+            let out = cryptonight(&asbytes, asbytes.len(), 0);
+            return hex::encode(out);
+        }
+    }
+}                                                                     
+
+fn main() {
+    let tc: u64 = 100;;
+    println!("Avrio Transaction Benchmark Version 0.1.0");
+    println!("Enter Number Of Txns To Generate And Validate");
+    println!("Generating {:?} txns", tc);
+    let txns = gen(tc);
+    println!("Done");
+    let now = Instant::now();
+    for tx in txns {
+        println!("Tx {:?}, valid: {}", tx.hash,tx.validateTransaction());
+    }
+    println!("Validated {:?} Transactions In {:?} Milliecconds. {:?} TPS", tc, now.elapsed().as_millis(), now.elapsed().as_millis() / (tc as u128));
+}
+
+
+
+
+fn hashBytes(asbytes: Vec<u8>) -> String{;
+    unsafe {
         let out = cryptonight(&asbytes, asbytes.len(), 0);
-        self.hash = hex::encode(out);
+        return hex::encode(out);
     }
 }
 
-fn hash(subject: String) -> String{
-    let asbytes = subject.as_bytes();
-    let out = cryptonight(&asbytes, asbytes.len(), 0);
-    return hex::encode(out);
-}
+fn hash(subject: String) -> String {
+    let asBytes = subject.as_bytes();
+    unsafe {
+        let out = cryptonight(&asBytes, asBytes.len(), 0);
+        return hex::encode(out);
+    }
+} 
