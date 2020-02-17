144bf44fa666f866da2b5964ced08f936367c611
Leo Cornelius <56726537+leocornelius@users.noreply.github.com>
2020-02-17 13:19:02 +0000

    Update main.rs

--- transaction/src/main.rs
+++ transaction/src/main.rs
@@ -73,7 +73,7 @@ fn gen(amount: u64) -> Result<Vec<Transaction>, ()> {
         };
         txn.hash();
         let pkcs8_bytes = signature::Ed25519KeyPair::generate_pkcs8(&rngc);
-        let key_pair = signature::Ed25519KeyPair::from_pkcs8(pkcs8_bytes.as_ref()).unwrap);
+        let key_pair = signature::Ed25519KeyPair::from_pkcs8(pkcs8_bytes.as_ref()).unwrap();
         // Sign the hash
         let msg: &[u8] = txn.hash.as_bytes();
         txn.signature = hex::encode(key_pair.sign(msg));
