7df3107b2fbe174c8e2b8edcd1815137c25174f0
Leo Cornelius <56726537+leocornelius@users.noreply.github.com>
2020-02-17 17:20:50 +0000

    Update main.rs

--- transaction/src/main.rs
+++ transaction/src/main.rs
@@ -126,10 +126,10 @@ impl Transaction {
                 return false;
             }
              else {
-                 let peer_public_key_bytes = hex::decode(&self.sender_key.to_owned).unwrap();
+                 let peer_public_key_bytes = hex::decode(&self.sender_key.to_owned().unwrap();
                  let peer_public_key =
                     signature::UnparsedPublicKey::new(&signature::ED25519, peer_public_key_bytes);
-                match peer_public_key.verify(self.hash, hex::decode(&txn.signature.to_owned()).unwrap()).unwrap() {
+                match peer_public_key.verify(self.hash.as_bytes(), &hex::decode(&txn.signature.to_owned()).unwrap()).unwrap() {
                     () => return true,
                     _ => return false,
                  }
