211487611a905905702142831d42cfc3568f051b
Leo Cornelius <leocornelius019@gmail.com>
2020-02-29 17:31:49 +0000

    Update

--- database/src/main.rs
+++ database/src/main.rs
@@ -1,7 +1,35 @@
-use avrio_database::*;
-
+use avrio_blockchain::Block;
+use avrio_core::Transaction;
+use avrio_database::{getData, saveData};
+use cryptonight::cryptonight;
+pub struct Chain {
+  public_key: String,
+  blocks: Vec<Block>,
+  digest: String,
+}
+const CHAIN_COUNT: u8 = 10;
+const BLOCKS_PER_CHAIN: u8 = 10;
 fn main() {
-  println!("hello world");
-  saveData("hi".to_string(), "./database.db".to_string(), "hey".to_string());
-  println!("{}", getData("./database.db".to_string(), "hey".to_string()));
+  let mut chains: Vec<Chain> = vec![];
+  for i in 0..= CHAIN_COUNT {
+    chains.push(Chain {
+      public_key: cryptonight("cisuhbfiw".to_string() + &i.to_string()),
+      blocks: vec![],
+      digest: "".to_string(),
+    });
+    for block_i in 0..=BLOCKS_PER_CHAIN {
+      chains[i].blocks.push(
+        Block::default
+      )
+    }
+  }
+  for chain in chains {
+    for block in chain.blocks {
+      saveData(
+        serde_json::to_string(&block),
+        "./".to_string() + &block.chain_key + ".db".to_string(),
+        block.hash,
+      );
+    }
+  }
 }
